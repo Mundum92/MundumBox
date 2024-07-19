bc01b2a544adb35c11ef62861ffa1ed48c4ee604
Mundum92 <nebravo23@gmail.com>
2024-07-19 18:25:05 +0300

    Топинамбур
    Топинамбур

--- homework1.py
+++ homework1.py
@@ -1,6 +1,6 @@
-num1=5
-num2=3
-sum=num1+num2
-print(sum)
-result=sum+num1*num2
-print(result)
+example='Топинамбур'
+print(example[0])
+print(example[-1])
+print(example[5:10])
+print(example[::-1])
+print(example[1:10:2])
