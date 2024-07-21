0d39c35239d6f5540f3f10bbb565f007e99fd292
Mundum92 <nebravo23@gmail.com>
2024-07-21 15:27:50 +0300

    Add files via upload
    переменные

--- homework2.py
+++ homework2.py
@@ -1,8 +1,9 @@
-name='Jen9'
-print(name)
-age=31
-print(age)
-New_age=32
-print(New_age)
-is_student=True
-print(is_student)
+homework_count=12
+homework_count_str=str(homework_count)
+time_count=1.5
+time_count_str=str(time_count)
+course_name='Python'
+time_for_one_task=time_count/homework_count
+time_for_one_task_str=str(time_for_one_task)
+print('Курс:'+course_name+',всего задач:'+ homework_count_str + ',затрчено часов:'+ time_count_str+',среднее время выполнения '+time_for_one_task_str +' часа.')
+#Курс: Python, всего задач:12, затрачено часов: 1.5, среднее время выполнения 0.125 часа.
