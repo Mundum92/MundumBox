c6051f63ce8181ccc2c924496519af9767a99ac7
Mundum92 <nebravo23@gmail.com>
2024-07-21 19:13:25 +0300

    метод
    метод

---
+++ homework4.py
@@ -0,0 +1,7 @@
+my_string=input('Привет Омлет')
+print(11) #отсчет с 0(тоесть 'П'-это 0)и последний символ не считается,пробел -символ
+my_string=input(('Привет омлет ').upper())
+my_string=input(('Привет омлет ').lower())
+my_string=input(('Привет омлет ').replace(' ',''))
+my_string=input(('Привет омлет ')[0])
+my_string=input(('Привет омлет ')[11])
